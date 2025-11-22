2ab8effdc8fcf72f425e0d411ee04c7113439448
cakeprograming <bolinhokwaikwaii@gmail.com>
2025-11-22 23:18:54 +0000

    ez

---
+++ example.lua
@@ -0,0 +1,11 @@
+local Library = loadstring(game:HttpGet(""))()
+
+local main = Library.new()
+
+local rage = main:create_tab('Blatant', 'rbxassetid://76499042599127')
+local player = main:create_tab('Player', 'rbxassetid://126017907477623')
+local world = main:create_tab('World', 'rbxassetid://85168909131990')
+local farm = main:create_tab('Farm', 'rbxassetid://132243429647479')
+local misc = main:create_tab('Misc', 'rbxassetid://132243429647479')
+
+main:load()
